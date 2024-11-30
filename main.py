78f00d1632d9f06bc28e1d539fb11750df1bb683
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-11-30 23:33:44 +0600

    Query Parameter

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ async def read_msg():
 @app.get('/greet/{name}')
 async def greet_name(name:str)->dict:
     return {"message": f"Hello {name}"}
+
+#Query parameter-->
+@app.get('/greet')
+async def greet_name(name:str)->dict:
+    return {"message": f"Hello {name}"}
