8d313853b37ae73c95e1aa0cab793825ef688893
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-11-30 23:22:33 +0600

    Custom get

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_msg():
     return ({"message":"new message"})
+
+@app.get('/greet/{name}')
+async def greet_name(name:str)->dict:
+    return {"message": f"Hello {name}"}
