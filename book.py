e15b23f70637ab4428cd50362161d52432e21ee0
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-04-29 23:29:10 +0600

    Pydantic

--- book.py
+++ book.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -15,12 +16,28 @@ class Book:
 
 
 Books = [
-    Book(1, "Math", "4"),
-    Book(2, "Bangla", "2"),
-    Book(3, "English", "3"),
+    Book(1, "Math", 4),
+    Book(2, "Bangla", 2),
+    Book(3, "English", 3),
 ]
 
 
 @app.get("/readbook")
 async def read_all():
     return Books
+
+
+# Pydantic model implementation
+
+
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    rating: int
+
+
+@app.post("/create_book")
+async def create_new_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    Books.append(new_book)
+    return {"message": "New Book appended successfully"}
