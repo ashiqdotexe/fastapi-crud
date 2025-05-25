2cd4e86dba73bc6b82d970b67e7a3d54b6fa6912
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-05-25 15:24:17 +0600

    Api request method

--- Todos/main.py
+++ Todos/main.py
@@ -1,6 +1,26 @@
 from database import engine
 import models
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from models import Todos
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close_all()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
