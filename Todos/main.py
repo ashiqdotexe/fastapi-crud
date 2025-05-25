b5692db5b85d7e8e7b2b0553a33b0f086b04e3b7
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-05-25 16:13:40 +0600

    Api request method-1

--- Todos/main.py
+++ Todos/main.py
@@ -1,10 +1,11 @@
 from database import engine
 import models
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from models import Todos
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -24,3 +25,11 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/")
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="No such todo id")
