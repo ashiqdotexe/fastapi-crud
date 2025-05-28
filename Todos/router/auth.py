e4b949dd68e21d365b1bee77f44cc821871fd70a
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-05-28 23:02:23 +0600

    Authentication-2

--- Todos/router/auth.py
+++ Todos/router/auth.py
@@ -6,9 +6,19 @@ from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def authenticate_user(user_name, password, db):
+    user = db.query(Users).filter(Users.user_name == user_name).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
 
 
 def get_db():
@@ -23,7 +33,10 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
-async def create_user(create_user_req: UserRequest, db: db_dependency):
+async def create_user(
+    create_user_req: UserRequest,
+    db: db_dependency,
+):
     new_user = Users(
         email=create_user_req.email,
         user_name=create_user_req.user_name,
@@ -35,3 +48,13 @@ async def create_user(create_user_req: UserRequest, db: db_dependency):
     )
     db.add(new_user)
     db.commit()
+
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def login_authentication(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Authentication Failed"
+    return "Successful authentication"
