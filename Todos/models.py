f48de6178a815e51e5e5784c7c1a345bf7aa0c12
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-05-28 22:50:45 +0600

    Authentication-1

--- Todos/models.py
+++ Todos/models.py
@@ -1,12 +1,12 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 
 
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String)
-    user_name = Column(String)
+    email = Column(String, unique=True)
+    user_name = Column(String, unique=True)
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
@@ -22,3 +22,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
--- Todos/router/auth.py
+++ Todos/router/auth.py
@@ -1,21 +1,37 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from starlette import status
 from schemas import UserRequest
 from models import Users
+from passlib.context import CryptContext
+from database import SessionLocal
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close_all()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
-async def create_user(create_user_req: UserRequest):
+async def create_user(create_user_req: UserRequest, db: db_dependency):
     new_user = Users(
         email=create_user_req.email,
         user_name=create_user_req.user_name,
         first_name=create_user_req.first_name,
         last_name=create_user_req.last_name,
-        hashed_password=create_user_req.password,
+        hashed_password=bcrypt_context.hash(create_user_req.password),
         is_active=True,
         role=create_user_req.role,
     )
-    if new_user:
-        return new_user
+    db.add(new_user)
+    db.commit()
