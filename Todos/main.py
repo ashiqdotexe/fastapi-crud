29c8e9e03e41e1dffd52d378b8085263b4a3b2d4
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-06 23:08:38 +0600

    Jinja2-1

--- Todos/main.py
+++ Todos/main.py
@@ -1,10 +1,18 @@
 from .database import engine
 from .models import Base
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .router import auth, admin, todos, users
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+template = Jinja2Templates(directory="Todos/templates")
+
+
+@app.get("/")
+def test(request: Request):
+    return template.TemplateResponse("home.html", {"request": request})
 
 
 app.include_router(auth.router)
