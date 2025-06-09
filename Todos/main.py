c751f4be0ed6bd5ffc81c9ff44a29c1637624bd3
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-09 23:38:30 +0600

    Full TODO Application

--- Todos/main.py
+++ Todos/main.py
@@ -2,19 +2,20 @@ from .database import engine
 from .models import Base
 from fastapi import FastAPI, Request
 from .router import auth, admin, todos, users
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from starlette import status
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
-template = Jinja2Templates(directory="Todos/templates")
+
 
 app.mount("/static", StaticFiles(directory="Todos/static"), name="static")
 
 
 @app.get("/")
 def test(request: Request):
-    return template.TemplateResponse("home.html", {"request": request})
+    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
 
 
 app.include_router(auth.router)
